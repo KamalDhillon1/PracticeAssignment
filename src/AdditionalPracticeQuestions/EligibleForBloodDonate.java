cca9e529caf5e3824a77777001272e966d149088
KamalDhillon <kkaur7887@gmail.com>
2022-01-07 11:23:47 -0800

    PracticeAssignment

---
+++ src/AdditionalPracticeQuestions/EligibleForBloodDonate.java
@@ -0,0 +1,23 @@
+package AdditionalPracticeQuestions;
+
+public class EligibleForBloodDonate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	    //age donate blood should be more than 18
+		//Weight should be more than 50kg
+		
+	int age=20;
+	int weight=62;
+	
+	if(age>18) {
+		//check weight
+	}if (weight>50) {
+		System.out.println("PersonIsEligible");
+		} else {
+			System.out.println("PersonIsnotEligible");
+		}
+	}
+
+}
---
+++ src/AdditionalPracticeQuestions/FindPositiveNegativeNumber.java
@@ -0,0 +1,17 @@
+package AdditionalPracticeQuestions;
+
+public class FindPositiveNegativeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	//More than zero is positive
+		//Less than zero is negative
+		int num=4;
+		if (num >0) {
+			System.out.println("num is a positive Number");
+		}else if (num <0) {
+			System.out.println("num is a negative Number");
+			}
+	}
+}
